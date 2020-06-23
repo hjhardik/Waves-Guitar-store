caf313a669857eb14e5bb8ec54a44a9c739d0b3e
Hardik Joshi <hardikj7321@gmail.com>
2020-06-24 00:22:08 +0530

    added proxy file

--- server/server.js
+++ server/server.js
@@ -572,7 +572,7 @@ if( process.env.NODE_ENV === 'production' ){
 }
 
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Server Running at ${port}`)
 });
