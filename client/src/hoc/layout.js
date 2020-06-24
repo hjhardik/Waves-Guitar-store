828bd953da859aaa3d650d90e9403135e48ed076
Hardik Joshi <hardikj7321@gmail.com>
2020-06-24 18:04:42 +0530

    updated layout.js

--- client/src/hoc/layout.js
+++ client/src/hoc/layout.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 
 import Header from '../components/Header_footer/Header';
 import Footer from '../components/Header_footer/Footer';
@@ -6,23 +6,26 @@ import Footer from '../components/Header_footer/Footer';
 import { connect } from 'react-redux';
 import { getSiteData } from '../actions/site_actions';
 
-function Layout (props) {
-    console.log('XXXXXXXXXXXXXXX',props);
-    useEffect(()=>{
-        if(Object.keys(props.site).length === 0){
-            props.dispatch(getSiteData());
-        }
-    });
+class Layout extends Component {
 
+
+    componentDidMount(){
+        if(Object.keys(this.props.site).length === 0){
+            this.props.dispatch(getSiteData());
+        }
+    }
+
+    render() {
         return (
             <div>
                 <Header/>
                 <div className="page_container">
-                    {props.children}
+                    {this.props.children}
                 </div>
-                <Footer data={props.site}/>
+                <Footer data={this.props.site}/>
             </div>
         );
+    }
 }
 
 const mapStateToProps = (state) => {
@@ -31,6 +34,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(Layout);    //The connect() function connects a React component to a Redux store.
-//It provides its connected component with the pieces of the data it needs from the store, and the functions it can use to dispatch actions to the store.
-//It does not modify the component class passed to it; instead, it returns a new, connected component class that wraps the component you passed in.
+export default connect(mapStateToProps)(Layout);
