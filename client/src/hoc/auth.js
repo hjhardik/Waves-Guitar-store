06b65a1325310cd7bc6e3c174fde4c5eac29ec66
Hardik Joshi <hardikj7321@gmail.com>
2020-06-24 18:01:15 +0530

    updated auth.js

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,44 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { auth } from '../actions/user_actions';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-export default function(ComposedClass,reload,adminRoute = null){ //composed class is the component passed //reload is the boolean var which is true for private routes, null for public and false for in between 
-    const AuthenticationCheck = (props) => {
-        const [loading,setLoading] = useState(true);  
+export default function(ComposedClass,reload,adminRoute = null){
+    class AuthenticationCheck extends Component {
 
-        useEffect(()=>{
-            props.dispatch(auth()).then(response => {
-                let user = props.user.userData;
+        state = {
+            loading: true
+        }
+
+        componentDidMount(){
+            this.props.dispatch(auth()).then(response =>{
+                let user = this.props.user.userData;
 
                 if(!user.isAuth){
                     if(reload){
-                        props.history.push('/register_login')
+                        this.props.history.push('/register_login')
                     }
                 } else{
                     if(adminRoute && !user.isAdmin){
-                        props.history.push('/user/dashboard')
+                        this.props.history.push('/user/dashboard')
                     } else{
                         if(reload === false){
-                            props.history.push('/user/dashboard')
+                            this.props.history.push('/user/dashboard')
                         }
                     }
                 }
-                setLoading(false)
+                this.setState({loading:false})
             })
-        });
-        
-        if(loading){ ////meanwhile when the useEffect is finished running, it will display circular loading 
-            return (
-                <div className="main_loader">
-                    <CircularProgress style={{color:'#2196F3'}} thickness={5}/> 
-                </div>
-            )
         }
-        //else
-        return (
-            <ComposedClass {...props} user={props.user}/>
+
+
+        render() {
+            if(this.state.loading){
+                return (
+                    <div className="main_loader">
+                        <CircularProgress style={{color:'#2196F3'}} thickness={7}/> 
+                    </div>
+                )
+            }
+            return (
+               <ComposedClass {...this.props} user={this.props.user}/>
             );
+        }
     }
 
     function mapStateToProps(state){
@@ -46,7 +51,8 @@ export default function(ComposedClass,reload,adminRoute = null){ //composed clas
             user: state.user
         }
     }
-    return connect(mapStateToProps)(AuthenticationCheck);
+
+    return connect(mapStateToProps)(AuthenticationCheck)
 }
 
 
